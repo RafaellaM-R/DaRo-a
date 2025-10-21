2c33e2e2862a267b584a70ea0d6c69461db0f17f
RafaellaM-R <rafamroque08@gmail.com>
2025-10-20 22:45:22 -0300

    Set up Express server with routes and middleware
    Refactor server.js to use Express and add routes.

--- API-DaRoca/server.js
+++ API-DaRoca/server.js
@@ -1,4 +1,27 @@
-const app = require("./app");
+require("dotenv").config(); // Carrega variáveis do .env
+const express = require("express");
+const cors = require("cors");
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(` API rodando em http://localhost:${PORT}`));
+// Importa as rotas
+const clienteRoutes = require("./routes/clienteRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+const perfilRoutes = require("./routes/perfilRoutes");
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use("/clientes", clienteRoutes);
+app.use("/auth", loginRoutes);
+app.use("/auth", loginRoutes);
+app.use("/perfil", perfilRoutes);
+
+// Porta definida no .env ou 3000 por padrão
+const PORT = process.env.PORT || 3000;
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(` Servidor rodando em http://localhost:${PORT}`);
+});
