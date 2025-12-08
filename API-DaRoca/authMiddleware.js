9bb00f73a194fcbe03563fc3da2ea7e651ea84bc
AnaGomes09 <ana.gos2009@gmail.com>
2025-12-08 20:47:56 -0300

    Delete API-DaRoca/authMiddleware.js

--- API-DaRoca/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.get("/dados", authMiddleware, (req, res) => {
-  res.json({ message: `Bem-vindo, ${req.usuario.email}!` });
-});
