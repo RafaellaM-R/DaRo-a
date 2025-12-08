5fea6ba64d71892bbc0445aa6b5c605837e20ff5
AnaGomes09 <ana.gos2009@gmail.com>
2025-12-08 20:37:19 -0300

    Refactor pedidoModel to use new database connection

--- API-DaRoca/models/pedidoModel.js
+++ API-DaRoca/models/pedidoModel.js
@@ -1,21 +1,69 @@
-const { sql, poolPromise } = require("../database/connection");
+const { conectaBD } = require("../config/db");
 
-async function listarPedidos() {
-  const pool = await poolPromise;
-  const result = await pool.request().query("SELECT * FROM daroca.pedidos");
-  return result.recordset;
-}
 
-async function inserirPedido(id_cliente, valor_total, status) {
-  const pool = await poolPromise;
-  await pool.request()
-    .input("id_cliente", sql.Int, id_cliente)
-    .input("valor_total", sql.Decimal(10, 2), valor_total)
-    .input("status", sql.VarChar(50), status || "Pendente")
-    .query(`
-      INSERT INTO daroca.pedidos (id_cliente, valor_total, status)
-      VALUES (@id_cliente, @valor_total, @status)
+
+async function inserir({ id_cliente, valor_total, status, frequencia, frete, itens }) {
+  const conexao = await conectaBD();
+
+  try {
+    
+    const pedidoResult = await conexao.query(`
+      INSERT INTO daroca.pedidos (id_cliente, valor_total, status, frequencia, frete)
+      OUTPUT INSERTED.id_pedido
+      VALUES (${id_cliente}, ${valor_total}, '${status}', '${frequencia}', ${frete})
     `);
+
+    const id_pedido = pedidoResult.recordset[0].id_pedido;
+
+   
+    for (const item of itens) {
+      await conexao.query(`
+        INSERT INTO daroca.PEDIDOS_ITENS (id_pedido, id_produto, quantidade)
+        VALUES (${id_pedido}, ${item.id_produto}, ${item.quantidade})
+      `);
+    }
+
+    return id_pedido;
+
+  } finally {
+    await conexao.close();
+  }
 }
 
-module.exports = { listarPedidos, inserirPedido };
+
+async function buscarPorCliente(idCliente) {
+  const conexao = await conectaBD();
+
+  const sql = `
+    SELECT 
+        p.id_pedido,
+        p.data_pedido,
+        p.valor_total,
+        p.status,
+
+        i.id_produto,
+        i.quantidade,
+
+        pr.nome AS nome_produto,
+        pr.valor AS preco_produto,
+        pr.imagem AS imagem
+
+    FROM daroca.pedidos p
+    JOIN daroca.PEDIDOS_ITENS i ON i.id_pedido = p.id_pedido
+    JOIN daroca.produtos pr ON pr.id = i.id_produto
+    WHERE p.id_cliente = ${idCliente}
+    ORDER BY p.id_pedido DESC
+  `;
+
+  try {
+    const result = await conexao.query(sql);
+    return result.recordset;
+  } catch (err) {
+    console.error(" erro no sql:", err);
+    throw new Error("Erro ao buscar pedidos do cliente.");
+  } finally {
+    await conexao.close();
+  }
+}
+
+module.exports = { inserir, buscarPorCliente };
