b05c13007b7f305518a22109340be6842b09e0e0
AnaGomes09 <ana.gos2009@gmail.com>
2025-12-08 20:30:31 -0300

    Refactor listarCategorias to use mssql directly

--- API-DaRoca/controllers/categoriaController.js
+++ API-DaRoca/controllers/categoriaController.js
@@ -1,12 +1,12 @@
-const { poolPromise } = require("../database/connection");
+const sql = require("mssql");
 
 async function listarCategorias(req, res) {
   try {
-    const pool = await poolPromise;
-    const result = await pool.request().query("SELECT * FROM daroca.categorias");
+    
+    const result = await sql.query("SELECT * FROM daroca.categorias");
     res.json(result.recordset);
   } catch (err) {
-    console.error(err);
+    console.error("Erro ao buscar categorias:", err);
     res.status(500).send("Erro ao buscar categorias: " + err.message);
   }
 }
