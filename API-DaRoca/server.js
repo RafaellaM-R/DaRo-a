7521b635df93d341617fbe4738ef64c98b23f52b
AnaGomes09 <ana.gos2009@gmail.com>
2025-12-08 20:46:19 -0300

    Refactor server setup and database connection
    Refactor server initialization to use app module and database connection.

--- API-DaRoca/server.js
+++ API-DaRoca/server.js
@@ -1,27 +1,14 @@
-require("dotenv").config(); // Carrega variáveis do .env
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
+const app = require("./app");
+const { conectaBD } = require("./config/db");
 
-// Importa as rotas
-const clienteRoutes = require("./routes/clienteRoutes");
-const loginRoutes = require("./routes/loginRoutes");
-const perfilRoutes = require("./routes/perfilRoutes");
-const app = express();
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Rotas
-app.use("/clientes", clienteRoutes);
-app.use("/auth", loginRoutes);
-app.use("/auth", loginRoutes);
-app.use("/perfil", perfilRoutes);
-
-// Porta definida no .env ou 3000 por padrão
-const PORT = process.env.PORT || 3000;
-
-// Inicia o servidor
-app.listen(PORT, () => {
-  console.log(` Servidor rodando em http://localhost:${PORT}`);
-});
+conectaBD()
+  .then(() => {
+    const PORT = process.env.PORT || 8081;
+    app.listen(PORT, () => {
+      console.log(` Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" Erro ao conectar ao banco:", err);
+  });
